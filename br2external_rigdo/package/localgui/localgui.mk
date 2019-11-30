182cf3e95ec21b75108f1c4898173783854cc96d
bond <bond@simicon.com>
2019-12-01 00:53:14 +0300

    fix install in br2external_rigdo/package/localgui/localgui.mk

--- br2external_rigdo/package/localgui/localgui.mk
+++ br2external_rigdo/package/localgui/localgui.mk
@@ -13,9 +13,9 @@ LOCALGUI_DEPENDENCIES = libwt
 
 define LOCALGUI_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/localgui $(TARGET_DIR)/root/localgui
-	$(INSTALL) -D -m 0644 $(@D)/css/style.css $(TARGET_DIR)/home/http/css/
-	$(INSTALL) -D -m 0644 $(@D)/webgui_ru.xml $(TARGET_DIR)/home/http/
-	$(INSTALL) -D -m 0644 $(@D)/webgui_en.xml $(TARGET_DIR)/home/http/
+	$(INSTALL) -D -m 0644 $(@D)/css/style.css $(TARGET_DIR)/home/http/css/style.css
+	$(INSTALL) -D -m 0644 $(@D)/webgui_ru.xml $(TARGET_DIR)/home/http/webgui_ru.xml
+	$(INSTALL) -D -m 0644 $(@D)/webgui_en.xml $(TARGET_DIR)/home/http/webgui_en.xml
 endef
 
 $(eval $(cmake-package))
