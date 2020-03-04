9f43bb0b99225484ac45999b14ddd8ce738e3132
bond <bond@simicon.com>
2020-03-05 00:39:57 +0300

    xmrig up version to 5.8.1

--- br2external_rigdo/package/xmrig/xmrig.mk
+++ br2external_rigdo/package/xmrig/xmrig.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-XMRIG_VERSION = 5.1.0
+XMRIG_VERSION = 5.8.1
 XMRIG_SITE = $(call github,xmrig,xmrig,v$(XMRIG_VERSION))
 
 XMRIG_DEPENDENCIES = libuv
